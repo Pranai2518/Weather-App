ec49d8b9797d37d275ddd0037621b60ec025d81a
Pranai Raj <75829770+Pranai2518@users.noreply.github.com>
2021-12-11 12:11:17 +0530

    Script update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let weather = {
+let weather;
+
+weather = {
     "apikey": "1e24e59781b24e5febe474826ebdfb1d",
-    fetchWeather: function() {
+    fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid" + this.apikey
         )
@@ -12,6 +14,31 @@ let weather = {
                 return response.json();
             })
             .then((data) => this.displayWeather(data));
-        },
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innertext = "weather in" + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innertext = description;
+        document.querySelector(".temp").innertext = temp + "°C";
+        document.querySelector(".humidity").innertext = "Humidity:" + humidity + "%" ;
+        document.querySelector(".speed").innertext = "Speed:" + speed + "km/h";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
+document.querySelector(".search button").addEventListener('click', function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Vijayawada");
